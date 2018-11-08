9ed647be787276b800826517f25e6960cdea5c2c
cruizg93 <crisruiz93@gmail.com>
2018-11-07 21:50:42 -0500

    Initial Commit for SimpleDefaultSecurity

---
+++ src/main/java/com/cristianruizblog/demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.cristianruizblog.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping
+	public String index() {
+		return "<b>Entramos</b>";
+	}
+}
